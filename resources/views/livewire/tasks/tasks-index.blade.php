71a108d4533d2ac3c31ed453de294f6faa99c7dc
4th-avenue <quebec706@gmail.com>
2024-08-28 20:47:18 +0900

    메뉴에 Tasks 추가하고 Tasks 입력 폼 생성

---
+++ resources/views/livewire/tasks/tasks-index.blade.php
@@ -0,0 +1,62 @@
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 text-gray-900">
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
+
+                    <!-- Title -->
+                    <div>
+                        <x-input-label for="title" :value="__('Title')" />
+                        <x-text-input id="title" class="block mt-1 w-full" type="text" name="title" :value="old('title')" required autofocus autocomplete="title" />
+                        <x-input-error :messages="$errors->get('title')" class="mt-2" />
+                    </div>
+
+                    <!-- Slug -->
+                    <div class="mt-4">
+                        <x-input-label for="slug" :value="__('Slug')" />
+                        <x-text-input id="slug" class="block mt-1 w-full" type="text" name="slug" :value="old('slug')" required autofocus autocomplete="slug" />
+                        <x-input-error :messages="$errors->get('slug')" class="mt-2" />
+                    </div>
+
+                    <!-- Description -->
+                    <div class="mt-4">
+                        <x-input-label for="description" :value="__('Description')" />
+                        <textarea id="description" rows="4" class="block mt-1 p-2.5 w-full text-sm text-gray-900 rounded-lg border border-gray-300 focus:ring-indigo-500 focus:border-indigo-500" placeholder="Leave a comment..."></textarea>
+                        <x-input-error :messages="$errors->get('description')" class="mt-2" />
+                    </div>
+
+                    <!-- Status -->
+                    <div class="mt-4">
+                        <label for="status" class="block text-sm font-medium text-gray-900 dark:text-white">Status</label>
+                        <select id="status" class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full mt-1 p-2.5">
+                            @foreach (\App\Enums\StatusType::cases() as $status)
+                                <option value="{{ $status->value }}">
+                                    {{ $status->name }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <!-- Priority -->
+                    <div class="mt-4">
+                        <label for="priority" class="block text-sm font-medium text-gray-900 dark:text-white">Priority</label>
+                        <select id="priority" class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full mt-1 p-2.5">
+                            @foreach (\App\Enums\PriorityType::cases() as $priority)
+                                <option value="{{ $priority->value }}">
+                                    {{ $priority->name }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <div class="flex items-center justify-end mt-4">
+                        <x-primary-button class="ms-4">
+                            {{ __('Submit') }}
+                        </x-primary-button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
