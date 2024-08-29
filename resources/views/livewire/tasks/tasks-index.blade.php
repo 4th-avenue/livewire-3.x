3ee6a07e3880b48b1dd6d18c174f2718a47845b2
4th-avenue <quebec706@gmail.com>
2024-08-29 12:53:21 +0900

    Task 저장 기능 구현

--- resources/views/livewire/tasks/tasks-index.blade.php
+++ resources/views/livewire/tasks/tasks-index.blade.php
@@ -2,34 +2,32 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
             <div class="p-6 text-gray-900">
-                <form method="POST" action="{{ route('register') }}">
-                    @csrf
-
+                <form wire:submit="save">
                     <!-- Title -->
                     <div>
                         <x-input-label for="title" :value="__('Title')" />
-                        <x-text-input id="title" class="block mt-1 w-full" type="text" name="title" :value="old('title')" required autofocus autocomplete="title" />
+                        <x-text-input wire:model="title" id="title" class="block mt-1 w-full" type="text" name="title" :value="old('title')" required autofocus autocomplete="title" />
                         <x-input-error :messages="$errors->get('title')" class="mt-2" />
                     </div>
 
                     <!-- Slug -->
                     <div class="mt-4">
                         <x-input-label for="slug" :value="__('Slug')" />
-                        <x-text-input id="slug" class="block mt-1 w-full" type="text" name="slug" :value="old('slug')" required autofocus autocomplete="slug" />
+                        <x-text-input wire:model="slug" id="slug" class="block mt-1 w-full" type="text" name="slug" :value="old('slug')" required autofocus autocomplete="slug" />
                         <x-input-error :messages="$errors->get('slug')" class="mt-2" />
                     </div>
 
                     <!-- Description -->
                     <div class="mt-4">
                         <x-input-label for="description" :value="__('Description')" />
-                        <textarea id="description" rows="4" class="block mt-1 p-2.5 w-full text-sm text-gray-900 rounded-lg border border-gray-300 focus:ring-indigo-500 focus:border-indigo-500" placeholder="Leave a comment..."></textarea>
+                        <textarea wire:model="description" id="description" rows="4" class="block mt-1 p-2.5 w-full text-sm text-gray-900 rounded-lg border border-gray-300 focus:ring-indigo-500 focus:border-indigo-500" placeholder="Leave a comment..."></textarea>
                         <x-input-error :messages="$errors->get('description')" class="mt-2" />
                     </div>
 
                     <!-- Status -->
                     <div class="mt-4">
                         <label for="status" class="block text-sm font-medium text-gray-900 dark:text-white">Status</label>
-                        <select id="status" class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full mt-1 p-2.5">
+                        <select wire:model="status" id="status" class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full mt-1 p-2.5">
                             @foreach (\App\Enums\StatusType::cases() as $status)
                                 <option value="{{ $status->value }}">
                                     {{ $status->name }}
@@ -41,13 +39,20 @@
                     <!-- Priority -->
                     <div class="mt-4">
                         <label for="priority" class="block text-sm font-medium text-gray-900 dark:text-white">Priority</label>
-                        <select id="priority" class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full mt-1 p-2.5">
+                        <select wire:model="priority" id="priority" class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full mt-1 p-2.5">
                             @foreach (\App\Enums\PriorityType::cases() as $priority)
                                 <option value="{{ $priority->value }}">
                                     {{ $priority->name }}
                                 </option>
                             @endforeach
                         </select>
+                    </div>
+
+                    <!-- Deadline -->
+                    <div class="mt-4">
+                        <x-input-label for="deadline" :value="__('Deadline')" />
+                        <x-text-input wire:model="deadline" id="deadline" class="block mt-1 w-full" type="date" name="deadline" :value="old('deadline')" required autofocus autocomplete="deadline" />
+                        <x-input-error :messages="$errors->get('deadline')" class="mt-2" />
                     </div>
 
                     <div class="flex items-center justify-end mt-4">
