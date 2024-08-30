4d566d41ddcb04029db1d2076720a40f31a01bc8
4th-avenue <quebec706@gmail.com>
2024-08-30 23:13:05 +0900

    Task 저장 후 TasksList 실시간 업데이트

--- app/Livewire/Tasks/TasksList.php
+++ app/Livewire/Tasks/TasksList.php
@@ -3,13 +3,16 @@
 namespace App\Livewire\Tasks;
 
 use Livewire\Component;
+use Livewire\Attributes\On;
 
 class TasksList extends Component
 {
+    #[On('task-created')]
+
     public function render()
     {
         return view('livewire.tasks.tasks-list', [
-            'tasks' => auth()->user()->tasks,
+            'tasks' => auth()->user()->tasks()->orderBy('created_at', 'desc')->get(),
         ]);
     }
 }
