d9fac2391865a15374ca283344763ae1789945b8
4th-avenue <quebec706@gmail.com>
2024-08-30 22:11:10 +0900

    TasksList 컴포넌트에서 직접 tasks 데이터를 가져오도록 로직 변경

--- app/Livewire/Tasks/TasksList.php
+++ app/Livewire/Tasks/TasksList.php
@@ -6,15 +6,10 @@ use Livewire\Component;
 
 class TasksList extends Component
 {
-    public $tasks;
-
-    public function mount($tasks)
-    {
-        $this->tasks = $tasks;
-    }
-
     public function render()
     {
-        return view('livewire.tasks.tasks-list');
+        return view('livewire.tasks.tasks-list', [
+            'tasks' => auth()->user()->tasks,
+        ]);
     }
 }
--- resources/views/livewire/tasks/tasks-index.blade.php
+++ resources/views/livewire/tasks/tasks-index.blade.php
@@ -1,6 +1,6 @@
 <div class="py-12">
     <div class="max-w-7xl mx-auto flex sm:px-6 lg:px-8">
-        <livewire:tasks.tasks-list :tasks="$tasks" />
+        <livewire:tasks.tasks-list />
         <livewire:tasks.tasks-form />
     </div>
 </div>
