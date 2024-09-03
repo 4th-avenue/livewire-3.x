c74cbfa34ab9fee33a029a88bf673ea97442a02c
4th-avenue <quebec706@gmail.com>
2024-09-03 17:21:51 +0900

    Alpine.js를 활용해 특정 키를 눌렀을 때 검색 입력 필드로 포커스가 이동

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,7 +1,12 @@
-<div class="relative w-96 max-w-lg px-1 pt-1">
-    <input wire:model.live.throttle.1000ms="search" type="text"
+<div class="relative w-96 max-w-lg px-1 pt-1" x-data="{
+    searchPosts(event) {
+        document.getElementById('searchInput').focus();
+        event.preventDefault();
+    }
+}">
+    <input wire:model.live.throttle.1000ms="search" type="text" id="searchInput"
         class="block w-full flex-1 py-2 px-3 mt-2 outline-none border-none rounded-md bg-slate-100"
-        placeholder="Start Typing..." />
+        placeholder="Start Typing..." @keydown.slash.window="searchPosts" />
     <div class="absolute mt-2 w-full overflow-hidden rounded-md bg-white">
         @foreach ($results as $result)
             <div class="cursor-pointer py-2 px-3 hover:bg-slate-100">
