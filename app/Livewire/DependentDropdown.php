7def4d168ded918cb4fc26cd0da6907e1f186f5c
4th-avenue <quebec706@gmail.com>
2024-09-04 17:53:22 +0900

    종속 드롭다운 기능 추가

--- app/Livewire/DependentDropdown.php
+++ app/Livewire/DependentDropdown.php
@@ -2,10 +2,36 @@
 
 namespace App\Livewire;
 
+use App\Models\City;
+use App\Models\State;
+use App\Models\Country;
 use Livewire\Component;
 
 class DependentDropdown extends Component
 {
+    public $countries;
+    public $states;
+    public $cities;
+
+    public $selectedCountry = null;
+    public $selectedState = null;
+
+    public function mount()
+    {
+        $this->countries = Country::all();
+    }
+
+    public function updatedSelectedCountry($country)
+    {
+        $this->states = State::where('country_id', $country)->get();
+        $this->selectedState = null;
+    }
+
+    public function updatedSelectedState($state)
+    {
+        $this->cities = City::where('state_id', $state)->get();
+    }
+
     public function render()
     {
         return view('livewire.dependent-dropdown')->layout('layouts.app');
--- resources/views/livewire/dependent-dropdown.blade.php
+++ resources/views/livewire/dependent-dropdown.blade.php
@@ -5,11 +5,44 @@
             <div class="mt-3">
                 <label for="country" class="block text-sm font-medium text-gray-900 dark:text-white">Country</label>
                 <div class="mt-2">
-                    <select id="country" class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full mt-1 p-2.5">
+                    <select wire:model.live="selectedCountry" id="country" class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full mt-1 p-2.5">
                         <option value="" selected>--Please choose an country--</option>
+                        @foreach($countries as $country)
+                            <option value="{{ $country->id }}">{{ $country->name }}</option>
+                        @endforeach
                     </select>
                 </div>
             </div>
+            @if(!is_null($selectedCountry))
+                <div class="mt-3">
+                    <label for="state" class="block text-sm font-medium text-gray-900 dark:text-white">State</label>
+                    <div class="mt-2">
+                        <select wire:model.live="selectedState" id="state" class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full mt-1 p-2.5">
+                            <option value="" selected>--Please choose an state--</option>
+                            @if($states)
+                                @foreach($states as $state)
+                                    <option value="{{ $state->id }}">{{ $state->name }}</option>
+                                @endforeach
+                            @endif
+                        </select>
+                    </div>
+                </div>
+            @endif
+            @if(!is_null($selectedState))
+                <div class="mt-3">
+                    <label for="city" class="block text-sm font-medium text-gray-900 dark:text-white">City</label>
+                    <div class="mt-2">
+                        <select id="city" class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full mt-1 p-2.5">
+                            <option value="" selected>--Please choose an city--</option>
+                            @if($cities)
+                                @foreach($cities as $city)
+                                    <option value="{{ $city->id }}">{{ $city->name }}</option>
+                                @endforeach
+                            @endif
+                        </select>
+                    </div>
+                </div>
+            @endif
         </div>
     </div>
 </div>
