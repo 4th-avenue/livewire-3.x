6d911199fd540981efd9146c1038d49fae3e5202
4th-avenue <quebec706@gmail.com>
2024-09-05 16:33:31 +0900

    종속 드롭다운 기능에 Select2 플러그인 추가

--- resources/views/livewire/dependent-dropdown.blade.php
+++ resources/views/livewire/dependent-dropdown.blade.php
@@ -2,12 +2,12 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="max-w-md mx-auto">
             <h1 class="text-2xl text-indigo-700 font-semibold">Livewire Example</h1>
-            <livewire:select-option name="Country" :options="$countries" wire:model.live="selectedCountry" />
+            <livewire:select-option name="Country" :options="$countries" wire:model.live="selectedCountry" :key="$countries->pluck('id')->join('-')" />
             @if(!is_null($selectedCountry))
-                <livewire:select-option name="State" :options="$states" wire:model.live="selectedState" />
+                <livewire:select-option name="State" :options="$states" wire:model.live="selectedState" :key="$states->pluck('id')->join('-')" />
             @endif
             @if(!is_null($selectedState))
-                <livewire:select-option name="City" :options="$cities" />
+                <livewire:select-option name="City" :options="$cities" :key="$cities->pluck('id')->join('-')" />
             @endif
         </div>
     </div>
--- resources/views/livewire/select-option.blade.php
+++ resources/views/livewire/select-option.blade.php
@@ -1,7 +1,7 @@
-<div class="mt-3">
-    <label for="{{$name}}" class="block text-sm font-medium text-gray-900 dark:text-white">{{ $name }}</label>
+<div class="mt-3" wire:ignore>
+    <label for="{{ $name }}" class="block text-sm font-medium text-gray-900 dark:text-white">{{ $name }}</label>
     <div class="mt-2">
-        <select wire:model.live="value" id="{{ $name }}" class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full mt-1 p-2.5">
+        <select id="{{ $name }}" class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full mt-1 p-2.5">
             <option value="" selected>--Please choose an {{ $name }}--</option>
             @foreach($options as $option)
                 <option value="{{ $option->id }}">{{ $option->name }}</option>
@@ -9,3 +9,25 @@
         </select>
     </div>
 </div>
+
+@assets
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+@endassets
+
+@script
+<script>
+    $(document).ready(function() {
+        $('#{{ $name }}').select2();
+
+        $('label[for="{{ $name }}"]').on('click', function() {
+            $('#{{ $name }}').select2('open');
+        });
+
+        $('#{{ $name }}').on('change', function(event) {
+            $wire.$set('value', event.target.value)
+        })
+    });
+</script>
+@endscript
